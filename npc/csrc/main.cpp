a512d670c42aacc685a2935398040690f4208cc9
tracer-ysyx <tracer@ysyx.org>
2023-09-12 11:07:34 +0800

    > sim RTL ysyx_noid Xiaohan Ni Linux cb 6.5.2-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 06 Sep 2023 21:01:01 +0000 x86_64 GNU/Linux  11:07:34 up  1:21,  1 user,  load average: 0.74, 0.34, 0.28

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -8,15 +8,24 @@ static TOP_NAME dut;
 void nvboard_bind_all_pins(Vtop *top);
 
 static void single_cycle() {
-  dut.a = rand() & 1;
-  dut.b = rand() & 1;
+  dut.clk = 0;
   dut.eval();
-  sleep(1);
+  dut.clk = 1;
+  dut.eval();
+}
+
+void reset(int n) {
+  dut.rst = 1;
+  while (n-- > 0)
+    single_cycle();
+  dut.rst = 0;
 }
 
 int main() {
   nvboard_bind_all_pins(&dut);
   nvboard_init();
+
+  reset(10);
 
   for (size_t i = 0; i < 10; ++i) {
     nvboard_update();
