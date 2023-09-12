2348b36ab0d9cb5a36435ceb9f12624c85f4cab1
tracer-ysyx <tracer@ysyx.org>
2023-09-12 12:39:36 +0800

    > sim RTL ysyx_noid Xiaohan Ni Linux cb 6.5.2-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 06 Sep 2023 21:01:01 +0000 x86_64 GNU/Linux  12:39:36 up  2:53,  1 user,  load average: 0.63, 0.46, 0.44

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -10,8 +10,10 @@ void nvboard_bind_all_pins(Vtop *top);
 static void single_cycle() {
   dut.clk = 0;
   dut.eval();
+  // sleep(1);
   dut.clk = 1;
   dut.eval();
+  // sleep(1);
 }
 
 void reset(int n) {
@@ -27,9 +29,12 @@ int main() {
 
   reset(10);
 
-  for (size_t i = 0; i < 10000; ++i) {
+  const size_t led_period = 5000000;
+  for (size_t i = 0; i < led_period * 4; ++i) {
     nvboard_update();
     single_cycle();
+    // single_cycle();
+    // single_cycle();
   }
   nvboard_quit();
 }
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,14 +1,23 @@
-module top(
-  input clk,
-  input rst,
-  output reg [15:0] led
+module top (
+    input clk,
+    input rst,
+    // input ps2_clk,
+    // input ps2_data,
+    output [15:0] led
 );
-  reg [31:0] count;
-  always @(posedge clk) begin
-    if (rst) begin led <= 1; count <= 0; end
-    else begin
-      if (count == 0) led <= {led[14:0], led[15]};
-      count <= (count >= 5000000 ? 32'b0 : count + 1);
-    end
-  end
+
+light light1(
+    .clk(clk),
+    .rst(rst),
+    .led(led)
+);
+
+
+// ps2_keyboard my_keyboard(
+//     .clk(clk),
+//     .resetn(~rst),
+//     .ps2_clk(ps2_clk),
+//     .ps2_data(ps2_data)
+// );
+
 endmodule
